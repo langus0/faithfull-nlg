5771cad0b52ae615810870b3febe6354d9d25a93
alxtrtw <alext45d@gmail.com>
2025-07-08 11:33:35 +0200

    Fix script for running error impact analysis

--- run_eval_impact.sh
+++ run_eval_impact.sh
@@ -3,7 +3,7 @@ uv run python src/eval_mod_impact.py \
 	--template src/templates/zero_shot/qags.jinja \
 	--data data/results/pregen_results/qags/pregen_eval_gemma.json \
 	--aspect-config src/configs/eval_aspects/qags-factual_consistency.json \
-	--output-dir data/results/eval_gemma_impact-1 \
+	--output-dir data/results/eval_gemma_impact1 \
 	--mod-direction 1 \
 
 uv run python src/eval_mod_impact.py \
