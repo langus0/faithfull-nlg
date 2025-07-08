c323ae0160cb53e502b1e652dcb800eca5638f1f
alxtrtw <alext45d@gmail.com>
2025-07-08 15:38:13 +0200

    Update severity script for simplification

--- run_eval_all_severities.sh
+++ run_eval_all_severities.sh
@@ -1,183 +1,67 @@
 MODEL=eval_gemma
+ASPECT=src/configs/eval_aspects/qags-factual_consistency.json
+TEMPLATE=src/templates/zero_shot/qags.jinja
 
-#### +1
-
-# int severity +1
+# if it is the first run for this set of OpeNLG parameters (model, aspect, template)
+# then run the first evaluation to generate the initial pregen (for example with severity modification 1)
 uv run python src/eval_mod.py \
 	--model ${MODEL} \
-	--template src/templates/zero_shot/qags.jinja \
+	--template ${TEMPLATE} \
+	--aspect-config  ${ASPECT}\
 	--data data/meta_eval/qags.json \
-	--aspect-config src/configs/eval_aspects/qags-factual_consistency.json \
 	--output-dir data/results/${MODEL}_severity1 \
 	--eval-mod severity \
 	--mod-force 1 \
 
-# save the basic evaluation in a pregen file (without the modification), which will be then reused by all following scripts
+# save the OpeNLG evaluation in a pregen file (without the severity modification)
+# it will be then used by following scripts to avoid generating OpeNLG evaluation again
 uv run python src/copy_results_to_pregen.py \
 	--results-path data/results/${MODEL}_severity1 \
 	--pregen-dest-path data/results/pregen_results/qags \
 	--model ${MODEL} \
 	--exclude-premodified-result
 
-# text severity +1 (using a previously pregenerated evaluation)
-uv run python src/eval_mod.py \
-	--model ${MODEL} \
-	--template src/templates/zero_shot/qags.jinja \
-	--data data/results/pregen_results/qags/pregen_${MODEL}.json \
-	--aspect-config src/configs/eval_aspects/qags-factual_consistency.json \
-	--output-dir data/results/${MODEL}_textsev1 \
-	--eval-mod text_severity \
-	--mod-force 1 \
+for sev_force in "${severities[@]}"
+do
+	echo "Running modifications with severity: $sev_force"
+  
+	# int severity (using a previously pregenerated evaluation)
+	uv run python src/eval_mod.py \
+		--model ${MODEL} \
+		--template ${TEMPLATE} \
+		--aspect-config  ${ASPECT}\
+		--data data/results/pregen_results/qags/pregen_${MODEL}.json \
+		--output-dir data/results/${MODEL}_severity${sev_force} \
+		--eval-mod severity \
+		--mod-force ${sev_force} \
 
-# now create a pregen file based on previous modification for the next joined modification
-uv run python src/copy_results_to_pregen.py \
-	--results-path data/results/${MODEL}_textsev1 \
-	--pregen-dest-path data/results/pregen_results/qags-textsev \
-	--model ${MODEL}_textsev1
+	# text severity (using a previously pregenerated evaluation)
+	uv run python src/eval_mod.py \
+		--model ${MODEL} \
+		--template ${TEMPLATE} \
+		--aspect-config  ${ASPECT}\
+		--data data/results/pregen_results/qags/pregen_${MODEL}.json \
+		--output-dir data/results/${MODEL}_textsev${sev_force} \
+		--eval-mod text_severity \
+		--mod-force ${sev_force} \
 
-# int and text severity +1
-# (using the newly pregenerated modification and its modified text of the results)
-uv run python src/eval_mod.py \
-	--model ${MODEL} \
-	--template src/templates/zero_shot/qags.jinja \
-	--data data/results/pregen_results/qags-textsev/pregen_${MODEL}_textsev1.json \
-	--aspect-config src/configs/eval_aspects/qags-factual_consistency.json \
-	--output-dir data/results/${MODEL}_int_and_textsev1 \
-	--eval-mod severity \
-	--mod-force 1 \
-	--use-premodified-result \
+	# now create a pregen file based on the text modification, which will be used in the next joined modification
+	uv run python src/copy_results_to_pregen.py \
+		--results-path data/results/${MODEL}_textsev${sev_force} \
+		--pregen-dest-path data/results/pregen_results/qags-textsev \
+		--model ${MODEL}_textsev${sev_force}
 
-
-
-
-
-#### +2
-
-# int severity +2
-# (using a previously pregenerated evaluation)
-uv run python src/eval_mod.py \
-	--model ${MODEL} \
-	--template src/templates/zero_shot/qags.jinja \
-	--data data/results/pregen_results/qags/pregen_${MODEL}.json \
-	--aspect-config src/configs/eval_aspects/qags-factual_consistency.json \
-	--output-dir data/results/${MODEL}_severity2 \
-	--eval-mod severity \
-	--mod-force 2 \
-
-# text severity +2
-# create a pregenerated modification (using a previously pregenerated evaluation)
-uv run python src/eval_mod.py \
-	--model ${MODEL} \
-	--template src/templates/zero_shot/qags.jinja \
-	--data data/results/pregen_results/qags/pregen_${MODEL}.json \
-	--aspect-config src/configs/eval_aspects/qags-factual_consistency.json \
-	--output-dir data/results/${MODEL}_textsev2 \
-	--eval-mod text_severity \
-	--mod-force 2 \
-
-# now create a pregen file based on previous modification for the next joined modification
-uv run python src/copy_results_to_pregen.py \
-	--results-path data/results/${MODEL}_textsev2 \
-	--pregen-dest-path data/results/pregen_results/qags-textsev \
-	--model ${MODEL}_textsev2
-
-# int and text severity +2
-# (using the newly pregenerated modification and its modified text of the results)
-uv run python src/eval_mod.py \
-	--model ${MODEL} \
-	--template src/templates/zero_shot/qags.jinja \
-	--data data/results/pregen_results/qags-textsev/pregen_${MODEL}_textsev2.json \
-	--aspect-config src/configs/eval_aspects/qags-factual_consistency.json \
-	--output-dir data/results/${MODEL}_int_and_textsev2 \
-	--eval-mod severity \
-	--mod-force 2 \
-	--use-premodified-result \
+	# int and text severity -1
+	# (using the newly pregenerated modification and its modified text of the results)
+	uv run python src/eval_mod.py \
+		--model ${MODEL} \
+		--template ${TEMPLATE} \
+		--aspect-config  ${ASPECT}\
+		--data data/results/pregen_results/qags-textsev/pregen_${MODEL}_textsev${sev_force}.json \
+		--output-dir data/results/${MODEL}_int_and_textsev${sev_force} \
+		--eval-mod severity \
+		--mod-force ${sev_force} \
+		--use-premodified-result \
 
 
-
-
-
-#### -1
-
-# int severity -1 (using a previously pregenerated evaluation)
-uv run python src/eval_mod.py \
-	--model ${MODEL} \
-	--template src/templates/zero_shot/qags.jinja \
-	--data data/results/pregen_results/qags/pregen_${MODEL}.json \
-	--aspect-config src/configs/eval_aspects/qags-factual_consistency.json \
-	--output-dir data/results/${MODEL}_severity-1 \
-	--eval-mod severity \
-	--mod-force -1 \
-
-# text severity -1 (using a previously pregenerated evaluation)
-uv run python src/eval_mod.py \
-	--model ${MODEL} \
-	--template src/templates/zero_shot/qags.jinja \
-	--data data/results/pregen_results/qags/pregen_${MODEL}.json \
-	--aspect-config src/configs/eval_aspects/qags-factual_consistency.json \
-	--output-dir data/results/${MODEL}_textsev-1 \
-	--eval-mod text_severity \
-	--mod-force -1 \
-
-# now create a pregen file based on previous modification for the next joined modification
-uv run python src/copy_results_to_pregen.py \
-	--results-path data/results/${MODEL}_textsev-1 \
-	--pregen-dest-path data/results/pregen_results/qags-textsev \
-	--model ${MODEL}_textsev-1
-
-# int and text severity -1
-# (using the newly pregenerated modification and its modified text of the results)
-uv run python src/eval_mod.py \
-	--model ${MODEL} \
-	--template src/templates/zero_shot/qags.jinja \
-	--data data/results/pregen_results/qags-textsev/pregen_${MODEL}_textsev-1.json \
-	--aspect-config src/configs/eval_aspects/qags-factual_consistency.json \
-	--output-dir data/results/${MODEL}_int_and_textsev-1 \
-	--eval-mod severity \
-	--mod-force -1 \
-	--use-premodified-result \
-
-
-
-
-
-
-#### -2
-
-# int severity -2 (using a previously pregenerated evaluation)
-uv run python src/eval_mod.py \
-	--model ${MODEL} \
-	--template src/templates/zero_shot/qags.jinja \
-	--data data/results/pregen_results/qags/pregen_${MODEL}.json \
-	--aspect-config src/configs/eval_aspects/qags-factual_consistency.json \
-	--output-dir data/results/${MODEL}_severity-2 \
-	--eval-mod severity \
-	--mod-force -2 \
-
-# text severity -2 (using a previously pregenerated evaluation)
-uv run python src/eval_mod.py \
-	--model ${MODEL} \
-	--template src/templates/zero_shot/qags.jinja \
-	--data data/results/pregen_results/qags/pregen_${MODEL}.json \
-	--aspect-config src/configs/eval_aspects/qags-factual_consistency.json \
-	--output-dir data/results/${MODEL}_textsev-2 \
-	--eval-mod text_severity \
-	--mod-force -2 \
-
-# now create a pregen file based on previous modification for the next joined modification
-uv run python src/copy_results_to_pregen.py \
-	--results-path data/results/${MODEL}_textsev-2 \
-	--pregen-dest-path data/results/pregen_results/qags-textsev \
-	--model ${MODEL}_textsev-2
-
-# int and text severity -2
-# (using the newly pregenerated modification and its modified text of the results)
-uv run python src/eval_mod.py \
-	--model ${MODEL} \
-	--template src/templates/zero_shot/qags.jinja \
-	--data data/results/pregen_results/qags-textsev/pregen_${MODEL}_textsev-2.json \
-	--aspect-config src/configs/eval_aspects/qags-factual_consistency.json \
-	--output-dir data/results/${MODEL}_int_and_textsev-2 \
-	--eval-mod severity \
-	--mod-force -2 \
-	--use-premodified-result \
+done
