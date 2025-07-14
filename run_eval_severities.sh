c8583e10317d3ce3fd3c89d0631d423d9bee84fd
alxtrtw <alext45d@gmail.com>
2025-07-15 00:42:06 +0200

    Fix in running scripts

--- run_eval_severities.sh
+++ run_eval_severities.sh
@@ -17,32 +17,32 @@ aspects=(coherence complexity relevance)
 
 severity_modification_forces=(1 2 -1 -2)
 
-TEMPLATE_PATH=src/templates/zero_shot/${DATASET}.jinja
-ASPECT_PATH=src/configs/eval_aspects/${DATASET}-${ASPECT}.json
-DATASET_PATH=data/meta_eval/${DATASET}.json
-RESULTS_DIR=results/eval_mod_results/${DATASET}/${ASPECT}
-PREGEN_DIR=results/pregen_results/${DATASET}/${ASPECT}
-
-# if it is the first run for this set of OpeNLG parameters (model, aspect, template)
-# then run the first evaluation to generate the initial pregen (for example with severity modification 1)
-uv run python src/eval_mod.py \
-	--model ${MODEL} \
-	--template ${TEMPLATE_PATH} \
-	--aspect-config  ${ASPECT_PATH}\
-	--data ${DATASET_PATH} \
-	--output-dir ${RESULTS_DIR}/${MODEL} \
-	--eval-mod none
-
-# save the OpeNLG evaluation in a pregen file (without the severity modification)
-# it will be then used by following scripts to avoid generating OpeNLG evaluation again
-uv run python src/copy_results_to_pregen.py \
-	--results-dir ${RESULTS_DIR}/${MODEL} \
-	--pregen-dest-dir ${PREGEN_DIR} \
-	--pregen-tag ${MODEL} \
-	--exclude-premodified-result
-
 for ASPECT in "${aspects[@]}"
 do
+
+	TEMPLATE_PATH=src/templates/zero_shot/${DATASET}.jinja
+	DATASET_PATH=data/meta_eval/${DATASET}.json
+	RESULTS_DIR=results/eval_mod_results/${DATASET}/${ASPECT}
+	PREGEN_DIR=results/pregen_results/${DATASET}/${ASPECT}
+
+	# if it is the first run for this set of OpeNLG parameters (model, aspect, template)
+	# then run the first evaluation to generate the initial pregen (for example with severity modification 1)
+	uv run python src/eval_mod.py \
+		--model ${MODEL} \
+		--template ${TEMPLATE_PATH} \
+		--aspect-config  ${ASPECT_PATH}\
+		--data ${DATASET_PATH} \
+		--output-dir ${RESULTS_DIR}/${MODEL} \
+		--eval-mod none
+
+	# save the OpeNLG evaluation in a pregen file (without the severity modification)
+	# it will be then used by following scripts to avoid generating OpeNLG evaluation again
+	uv run python src/copy_results_to_pregen.py \
+		--results-dir ${RESULTS_DIR}/${MODEL} \
+		--pregen-dest-dir ${PREGEN_DIR} \
+		--pregen-tag ${MODEL} \
+		--exclude-premodified-result
+
 	for sev_force in "${severity_modification_forces[@]}"
 	do
 		echo "Running modifications with severity: $sev_force"
