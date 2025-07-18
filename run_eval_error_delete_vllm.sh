c79b964888ca0b50c0ca498d45af1396997d1ac5
alxtrtw <alext45d@gmail.com>
2025-07-18 15:10:26 +0200

    Update scripts for addition and deletion

--- run_eval_error_delete_vllm.sh
+++ run_eval_error_delete_vllm.sh
@@ -1,17 +1,18 @@
 # MODEL=eval_nemo
-MODEL=eval_gemma
+# MODEL=eval_gemma
 # MODEL=eval_qwen
 # MODEL=eval_mistral
+models=(eval_nemo eval_gemma eval_qwen)
 
 # DATASET=qags
-DATASET=hanna
-# DATASET=summeval
+# DATASET=hanna
+DATASET=summeval
 
 # ASPECT=factual_consistency
 # ASPECT=coherence
 # ASPECT=relevance
 # ASPECT=complexity
-aspects=(coherence complexity relevance)
+aspects=(coherence factual_consistency complexity)
 
 # set to -1 for deletion of current error and all previous ones
 # set to 1 for deletion of current error and all further ones
@@ -19,24 +20,28 @@ aspects=(coherence complexity relevance)
 # DELETION_CASCADE=-1
 deletion_cascades=(0 1 -1)
 
-for DELETION_CASCADE in "${deletion_cascades[@]}"
+for MODEL in "${models[@]}"
 do
 	for ASPECT in "${aspects[@]}"
 	do
-		TEMPLATE_PATH=src/templates/zero_shot/${DATASET}.jinja
-		ASPECT_PATH=src/configs/eval_aspects/${DATASET}-${ASPECT}.json
-		PREGEN_DIR=results2/pregen_results/${DATASET}/${ASPECT}
-		RESULTS_DIR=results2/eval_mod_results/${DATASET}/${ASPECT}
+		for DELETION_CASCADE in "${deletion_cascades[@]}"
+		do
+			TEMPLATE_PATH=src/templates/zero_shot/${DATASET}.jinja
+			ASPECT_PATH=src/configs/eval_aspects/${DATASET}-${ASPECT}.json
+			PREGEN_DIR=results2/pregen_results/${DATASET}/${ASPECT}
+			RESULTS_DIR=results2/eval_mod_results/${DATASET}/${ASPECT}
 
-		echo "Running error deletion tests ${DELETION_CASCADE} for aspect: $ASPECT"
-		uv run python src/eval_mod_per_error_vllm.py \
-			--model ${MODEL} \
-			--template ${TEMPLATE_PATH} \
-			--aspect-config ${ASPECT_PATH} \
-			--data ${PREGEN_DIR}/pregen_${MODEL}.json \
-			--output-dir ${RESULTS_DIR}/${MODEL}_delete${DELETION_CASCADE} \
-			--per_error_mod delete \
-			--mod-direction ${DELETION_CASCADE}
+			echo "Running error deletion ${DELETION_CASCADE} for $DATASET-$ASPECT using model $MODEL"
 
+			uv run python src/eval_mod_per_error_vllm.py \
+				--model ${MODEL} \
+				--template ${TEMPLATE_PATH} \
+				--aspect-config ${ASPECT_PATH} \
+				--data ${PREGEN_DIR}/pregen_${MODEL}.json \
+				--output-dir ${RESULTS_DIR}/${MODEL}_delete${DELETION_CASCADE} \
+				--per_error_mod delete \
+				--mod-direction ${DELETION_CASCADE}
+
+		done
 	done
 done
