e7f86ffd1fc8374c590b13bfe519ad20c3d9e4ec
Mateusz <mateusz@ufal>
2025-07-19 07:17:38 +0200

    p2

--- run_calculate_plots.sh
+++ run_calculate_plots.sh
@@ -14,20 +14,18 @@ ASPECT=factual_consistency
 
 
 
-severity_modification_types=(severity textsev int_and_textsev)
 
-severity_modification_forces=(1 2 -1 -2)
 
-for MODEL in eval_nemo eval_gemma; do
-    for ASPECT in relevance coherence factual_consistency; do
-        RESULTS_DIR=results3/eval_mod_results/${DATASET}/${ASPECT}
+for MODEL in eval_nemo eval_gemma eval_qwen; do
+    for ASPECT in factual_consistency relevance coherence; do
+        RESULTS_DIR=results2/eval_mod_results/${DATASET}/${ASPECT}
         printf "\nRunning modifications for model ${MODEL} on aspect ${ASPECT}\n"
 # for mod_type in "${severity_modification_types[@]}"
 # do
 #     for sev_force in "${severity_modification_forces[@]}"
 #     do
         printf "\nMetrics for modification ${mod_type}${sev_force}\n"
-        python src/calculate_plots.py \
+        python src/calculate_plots2.py \
             --results-dir ${RESULTS_DIR}/${MODEL}
     done
 done
